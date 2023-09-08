38cb307527f11c03f4756f7baac1e68ecee5c1f2
Helixjoe <joeljohn5112@gmail.com>
2023-09-09 00:48:23 +0530

    notes data added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from "react"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Note from "./components/Note"
+import notes from "./notes"
+
 function App() {
     return <div><Header />
-        <Note />
+        {notes.map((info) =>
+            <Note key={info.id} content={info.content} title={info.title} />)
+        }
         <Footer /></div>
 }
 
